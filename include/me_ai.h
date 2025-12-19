168d5c091779fbc939ff79aec687a22c5aaeab03
Houssam BACAR <houssambacar67@gmail.com>
2025-12-19 22:06:29 +0100

    Correction de quelques erreurs

---
+++ include/me_ai.h
@@ -0,0 +1,11 @@
+#ifndef __ME_AI_H__
+#define __ME_AI_H__
+
+typedef struct {
+	void (*evaluate)(void *);
+} ME_AI;
+
+
+void me_init(ME_AI * me_ai);
+
+#endif
---
+++ src/me_ai.c
@@ -0,0 +1,5 @@
+#include "../include/me_ai.h"
+
+void me_init(ME_AI * me_ai){
+	
+}
